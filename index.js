b07d63a421c0cb146e92fe8844116d495ab982ba
Rafael-Patricio <rafaelpatricio1@outlook.com>
2024-09-04 19:31:22 -0300

    iniciando repositório git

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1 - criar lista vazia
+let listaJogos = [890, 'Crash 3', 'Ace Combat']
+
+let listaEstrelas = [{nome: 'Angelo', idade: 12}, 'Betegeuase', 'Sirius', 'Anilam', 12]
+
+// 2 - Alterar o último elemento para maiúsculo
+
+console.log(listaJogos[2].toUpperCase())
+
+// 3 - Alterar todos os elementos para maiúsculo
